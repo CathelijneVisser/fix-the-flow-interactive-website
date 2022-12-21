94b80f31349354e940961ec20094991e3c685a3f
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2022-12-21 13:33:41 +0100

    noooooooooooooo

--- scripts/script.js
+++ scripts/script.js
@@ -3,15 +3,26 @@ const allButtons = document.querySelectorAll(".js-expand");
 const menuClosed = document.querySelector(".menu-button");
 const nav = document.querySelector(".nav");
 const WWvergeten = document.querySelector("button.ww-vergeten");
-const WwvergetenP = document.querySelector("p.ww-vergeten")
+const WwvergetenP = document.querySelector("p.ww-vergeten");
+const RegisterOpen = document.querySelector(".register-link");
+const Register = document.querySelector("#register");
+const RegisterClose = document.querySelector(".login-cross-register");
+const Login = document.querySelector(".login-container");
+const RegisterButton = document.querySelector(".register-button");
 
 BackHome.addEventListener("click", () => {
-	window.location,href = "index,html"
+	window.location((href = "index.html"));
 });
 
+RegisterClose.addEventListener("click", RegisterGoBack);
+
+RegisterOpen.addEventListener("click", RegisterGo);
+
+RegisterButton.addEventListener("click", RegisterGoBack);
+
 WWvergeten.addEventListener("click", () => {
-	WwvergetenP.classList.toggle("visible")
-})
+	WwvergetenP.classList.toggle("visible");
+});
 
 allButtons.forEach((button) => {
 	button.addEventListener("click", () => {
@@ -27,4 +38,16 @@ if (menuClosed) {
 	nav.addEventListener("click", () => {
 		nav.classList.toggle("open");
 	});
+}
+
+function RegisterGo() {
+	Register.classList.toggle("expand");
+	setTimeout(() => {
+		Login.classList.toggle("hidden");
+	}, 1000);
+}
+
+function RegisterGoBack() {
+	Register.classList.toggle("expand");
+	Login.classList.toggle("hidden");
 }
